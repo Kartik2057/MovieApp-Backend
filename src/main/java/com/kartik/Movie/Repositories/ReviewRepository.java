4aee393eacd06683d0868e89454c420493042333
Kartik2057 <2021kuec2057@iiitkota.ac.in>
2023-10-24 12:32:50 +0530

    First Commit

---
+++ src/main/java/com/kartik/Movie/Repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.kartik.Movie.Repositories;
+
+import com.kartik.Movie.Models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ src/main/java/com/kartik/Movie/Services/ReviewService.java
@@ -0,0 +1,26 @@
+package com.kartik.Movie.Services;
+
+import com.kartik.Movie.Models.Movie;
+import com.kartik.Movie.Models.Review;
+import com.kartik.Movie.Repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        return review;
+    }
+}
